9ad002a6196b8fd4a918b16e32af70615031de7d
Blake Reimer <sobeksea@gmail.com>
2020-06-22 23:44:27 -0600

    Pull queries into index.js

--- queries.js
+++ queries.js
@@ -12,7 +12,7 @@ const getUsers = (request, response) => {
     if (error) {
       throw error
     }
-    response.status(200).json(results.row)
+    response.status(200).json(results.rows)
   })
 }
 
